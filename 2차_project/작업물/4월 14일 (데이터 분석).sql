7f2b4585e3327594f684a320abff6e98e60f54ae
spiner321 <96769262+spiner321@users.noreply.github.com>
2022-06-23 17:02:23 +0900

    Delete 2차_project directory

--- 2차_project/작업물/4월 14일 (데이터 분석).sql
+++
@@ -1,3 +0,0 @@
--- # 원자재 확인
-SELECT DISTINCT 원자재코드, 원자재명 FROM production_recipe
-order BY 원자재코드;
--- 2차_project/작업물/4월 16일 (데이터 분석).sql
+++
@@ -1,3 +0,0 @@
--- 2월 간 수주를 받은 거래처
-SELECT DISTINCT 거래처코드 FROM contract
-WHERE 수주일자 BETWEEN '2021-02-22' AND '2021-02-28';
